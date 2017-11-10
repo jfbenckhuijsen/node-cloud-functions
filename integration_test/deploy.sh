5421604fc6977e969d0dc419946d1cf10787098d
jeroen.benckhuijsen@group9.nl <jeroen.benckhuijsen@group9.nl>
2017-11-10 20:18:23 +0100

    Added console logging

--- integration_test/deploy.sh
+++ integration_test/deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Deploying function $2 from directory $1. Trigger will be: $4 $5"
+echo "Using stage bucket $3 for deployment"
+
 cd $1
-gcloud alpha functions deploy $2 --stage-bucket $3 --trigger-http > deploy_result.log
+gcloud alpha functions deploy $2 --stage-bucket $3 $4 $5 > deploy_result.log
 
 grep "  url" deploy_result.log | awk '{print $2}' > deploy_url
 
