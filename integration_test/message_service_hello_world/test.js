8621d4861835a5c05fead4a500307dd38d7db7b3
Jeroen Benckhuijsen <jeroen.benckhuijsen@group9.nl>
2022-06-03 10:38:25 +0200

    Fix async testing

--- integration_test/message_service_hello_world/test.js
+++ integration_test/message_service_hello_world/test.js
@@ -1,21 +1,16 @@
 module.exports = (it, runnerStrategy, expect, config) => {
-  it('--> should perform a basic hello world call', async (done) => {
-    let reply;
-    await runnerStrategy.messageClient(config.replyTopic, (message) => {
+  it('--> should perform a basic hello world call', () => new Promise((resolve, reject) => {
+    runnerStrategy.messageClient(config.replyTopic, (message) => {
       console.log(`***** Received reply from topic: ${message.data}`);
-      reply = message.data.toString();
-    });
-
-    const message = config.replyTopic;
-    runnerStrategy.messageSender(config.topic, message);
-
-    setTimeout(() => {
-      console.log('***** Wait timeout reached, checking if reply has been received');
-      expect(reply).to.not.be.null;
-      expect(reply)
-        .to
-        .equal(config.replyTopic);
-      done();
-    }, 10000);
-  });
+      const reply = message.data.toString();
+      if (reply === config.replyTopic) {
+        resolve();
+      } else {
+        reject(new Error(`reply is not the replyTopic, but ${reply}`));
+      }
+    }).then(() => {
+      const message = config.replyTopic;
+      runnerStrategy.messageSender(config.topic, message);
+    }).catch((err) => reject(err));
+  }));
 };
